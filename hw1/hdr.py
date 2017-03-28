6c3d455e8476f319d86ab349c4a23a88c91c5eee
code14 <mob5566@gmail.com>
2017-03-28 21:45:58 +0800

    Reconstruct respondse function

--- hw1/hdr.py
+++ hw1/hdr.py
@@ -14,6 +14,46 @@ import sys
 import cv2
 import numpy as np
 
+# Triangular hat weighting function
+w = lambda(z): float(z) if z<=127 else float(255-z)
+
+# Fill the Sparse linear system
+def getOptimizeMatrix(imgs, expts, pts, lamb):
+  nImgs = imgs.shape[0]
+  nPts = len(pts)
+
+  # Sparse matrix
+  A = np.zeros((nImgs*nPts+255, 256+nPts), np.float32)
+  b = np.zeros((nImgs*nPts+255, 1), np.float32)
+
+  k = 0
+  for j, img in enumerate(imgs):
+    for i, pt in enumerate(pts):
+      z = img[pt]
+      A[k, z] = w(z)
+      A[k, i+256] = w(z)
+      b[k] = np.log(expts[j])*w(z)
+      k += 1
+  A[k, 127] = 1.
+  k += 1
+
+  for i in xrange(254):
+    A[k, i] = w(i+1)*lamb
+    A[k, i+1] = -2.*w(i+1)*lamb
+    A[k, i+2] = w(i+1)*lamb
+    k+=1
+
+  return A, b
+
+# Calculate the respondse function
+def getRespondseFunction(A, b):
+  invA = np.linalg.pinv(A)
+  x = np.dot(invA, b)
+  g = x[:256]
+  le = x[256:]
+
+  return g, le
+
 # Default parameters
 outImage = 'img.hdr'
 nPoints = 50
@@ -29,23 +69,32 @@ else:
 
 # Get the name of images and its exposure time
 imgs = []
+expts = []
 
 with open(inFile, 'rb') as f:
   for line in f.readlines():
     fn, exptime = line.split()
     exptime = float(exptime)
 
-    imgs.append((fn, exptime))
+    imgs.append(cv2.imread(fn))
+    expts.append(exptime)
 
-# Get an image for demonstration
-img = cv2.imread(imgs[0][0])
+imgs = np.array(imgs)
 
 # Generate random points
-imgHeight, imgWidth, imgChannels = img.shape
+imgHeight, imgWidth, imgChannels = imgs[0].shape
 
-pts = [(np.random.randint(0, imgWidth), np.random.randint(0, imgHeight))
+pts = [(np.random.randint(0, imgHeight), np.random.randint(0, imgWidth))
        for _ in xrange(nPoints)]
 
-for pt in pts:
-  cv2.circle(img, tuple(pt), 3, (0, 0, 255))
+# Fill the optimization matrix
+Ar, br = getOptimizeMatrix(imgs[:, :, :, 0].reshape(-1, imgHeight, imgWidth), expts, pts, 10.)
+Ag, bg = getOptimizeMatrix(imgs[:, :, :, 1].reshape(-1, imgHeight, imgWidth), expts, pts, 10.)
+Ab, bb = getOptimizeMatrix(imgs[:, :, :, 2].reshape(-1, imgHeight, imgWidth), expts, pts, 10.)
+
+# Solve the optimization eqation by SVD
+gr, ler = getRespondseFunction(Ar, br)
+gg, leg = getRespondseFunction(Ag, bg)
+gb, leb = getRespondseFunction(Ab, bb)
+
 
