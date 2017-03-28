09cf7e9868a4eb43db572fe384a92922ef842997
code14 <mob5566@gmail.com>
2017-03-28 16:08:07 +0800

    Generate random points

--- hw1/hdr.py
+++ hw1/hdr.py
@@ -1,10 +1,10 @@
-"""
+'''
 # hdr.py -- Calculate HDR image with given image list file
 
 * for digital visual effects homework1
 * generate a hdr image by <image-list>
 
-"""
+'''
 
 from __future__ import print_function
 
@@ -12,8 +12,11 @@ import os
 import sys
 
 import cv2
+import numpy as np
 
+# Default parameters
 outImage = 'img.hdr'
+nPoints = 50
 
 if len(sys.argv)!=2 and len(sys.argv)!=3:
   print('    Usage: python hdr.py <image list file> [hdr image]')
@@ -34,4 +37,15 @@ with open(inFile, 'rb') as f:
 
     imgs.append((fn, exptime))
 
+# Get an image for demonstration
+img = cv2.imread(imgs[0][0])
+
+# Generate random points
+imgHeight, imgWidth, imgChannels = img.shape
+
+pts = [(np.random.randint(0, imgWidth), np.random.randint(0, imgHeight))
+       for _ in xrange(nPoints)]
+
+for pt in pts:
+  cv2.circle(img, tuple(pt), 3, (0, 0, 255))
 
