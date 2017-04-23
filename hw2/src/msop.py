52183b9e7bfc4158b9b9888c6df55748f2f38686
code14 <mob5566@gmail.com>
2017-04-23 21:39:57 +0800

    Add feature descriptor

--- hw2/src/msop.py
+++ hw2/src/msop.py
@@ -20,11 +20,13 @@ from itertools import product
 eps = 1e-8
 
 class MSOP(object):
-  def __init__(self, numFeat=500, pyrLevel=3, fhmt=10.0):
-    self.nf = numFeat       # Number of features
-    self.pyrl = pyrLevel    # Number of levels of pyramid
-    self.ft = fhmt          # Harmonic mean threshold
-    self.kp = None          # Keypoints
+  def __init__(self, numFeat=500, pyrLevel=3, fhmt=10.0, samplePatchSize=8, sampleSpace=5):
+    self.nf = numFeat           # Number of features
+    self.pyrl = pyrLevel        # Number of levels of pyramid
+    self.ft = fhmt              # Harmonic mean threshold
+    self.kp = None              # Keypoints
+    self.sps = samplePatchSize  # Sample Patch Size
+    self.ss = sampleSpace       # Sample space
   
   def detect(self, img):
 
@@ -73,7 +75,7 @@ class MSOP(object):
         fhm[y, x] = (hl[0, 0]*hl[1, 1])/(hl[0, 0]+hl[1, 1]+eps)
 
       # Detect key points
-      for x, y in product(range(1, cw-1), range(1, ch-1)):
+      for x, y in product(range(30, cw-30), range(30, ch-30)):
         nei = fhm[y-1:y+2, x-1:x+2]
         if fhm[y, x]>=self.ft and np.all(fhm[y, x]>=nei.reshape(-1)):
           sx, sy = MSOP.spa((x, y), nei)
@@ -86,24 +88,6 @@ class MSOP(object):
 
     return self.kp
 
-  # Sub-pixel Accuracy
-  @staticmethod
-  def spa(p, neig):
-
-    # Derivatives
-    dx = (neig[1, 2]-neig[1, 0])/2.0
-    dy = (neig[2, 1]-neig[0, 1])/2.0
-    dxx = neig[1, 2]-2.0*neig[1, 1]+neig[1, 0]
-    dyy = neig[2, 1]-2.0*neig[1, 1]+neig[0, 1]
-    dxy = ((neig[2, 2]-neig[2, 0])-(neig[0, 2]-neig[0, 0]))/4.0
-
-    # First-order and Second-order derivative
-    fd = np.array([[dx], [dy]])
-    sd = np.array([[dxx, dxy], [dxy, dyy]])
-
-    pm = -np.dot(np.linalg.inv(sd), fd)
-    return (p[0]+pm[0], p[1]+pm[1])
-  
   # Adaptive Non-Maximal Suppression
   def anms(self):
     if not self.kp:
@@ -131,24 +115,100 @@ class MSOP(object):
 
     newkp.sort(key=lambda x: x[4], reverse=True)
 
-    return newkp[:self.nf]
+    self.kp = [(x, y, l, d) for x, y, l, d, r in newkp[:self.nf]]
+    return self.kp
 
-def main():
-  img = cv2.imread('data/parrington/prtn00.jpg')
+  # Compute the descriptor of features
+  def descriptor(self):
+    desp = []
+    gk = cv2.GaussianBlur
+    ksize = (5, 5)
+    ppyramid = [gk(gk(pyr.copy(), ksize, 1.0), ksize, 1.0) for pyr in self.pyramid]
 
-  fd = MSOP()
-  kp = fd.detect(img)
-  nkp = fd.anms()
+    patchSize = (self.sps*self.ss, self.sps*self.ss)
+    featSize = (self.sps, self.sps)
 
-  # Show detected key points
-  for p in nkp:
-    cv2.circle(img, (p[0]*2**p[2], p[1]*2**p[2]), 2*2**p[2], (0, 0, 255))
-    cv2.line(img, (p[0]*2**p[2], p[1]*2**p[2]), ((p[0]+p[3][0]*2)*2**p[2], (p[1]+p[3][1]*2)*2**p[2]), (0, 255, 0), 2)
+    # Get the patch of each keypoint and rotate it with main gradient
+    for i, p in enumerate(self.kp):
+      x, y, l, (dx, dy) = p
+      pimg = ppyramid[l]
+      ch, cw = pimg.shape
+      theta = np.arctan2(-dy, dx)*180/np.pi
+      
+      M = cv2.getRotationMatrix2D((x, y), -theta, 1.)
+      trans = cv2.warpAffine(pimg, M, (cw, ch))
+      patch = cv2.getRectSubPix(np.round(trans).astype(np.uint8), patchSize, (x, y))
+      patch = cv2.resize(patch, featSize)
 
-  cv2.imwrite('kp_prtn00.jpg', img)
+      des = patch.reshape(-1).astype(np.float)
+      des = (des-des.mean())/(des.std()+eps)
 
-  return kp, nkp
+      desp.append(des)
+
+    self.desp = np.array(desp)
+    return self.desp
+
+  # Find keypoints and compute descriptor
+  def detectAndDescribe(self, img):
+    self.detect(img)
+    kp = self.anms()
+    desp = self.descriptor()
+
+    return kp, desp
+
+  # Draw keypoints
+  @staticmethod
+  def drawKeypoints(img, kp, top=30, cc=(0, 0, 255), cl=(255, 0, 0)):
+    assert(len(cc)==3)
+    cc = tuple(cc)
+    assert(len(cl)==3)
+    cl = tuple(cl)
+    for p in kp[:top]:
+      x, y, l, (dx, dy) = p
+
+      toi = lambda v: int(round(v))
+      cx1 = toi(x*2**l)
+      cy1 = toi(y*2**l)
+      cx2 = toi((x+dx*20)*2**l)
+      cy2 = toi((y+dy*20)*2**l)
+
+      cv2.circle(img, (cx1, cy1), 20*2**l, cc)
+      cv2.line(img, (cx1, cy1), (cx2, cy2), cl, 1)
+    
+  # Sub-pixel Accuracy
+  @staticmethod
+  def spa(p, neig):
+
+    # Derivatives
+    dx = (neig[1, 2]-neig[1, 0])/2.0
+    dy = (neig[2, 1]-neig[0, 1])/2.0
+    dxx = neig[1, 2]-2.0*neig[1, 1]+neig[1, 0]
+    dyy = neig[2, 1]-2.0*neig[1, 1]+neig[0, 1]
+    dxy = ((neig[2, 2]-neig[2, 0])-(neig[0, 2]-neig[0, 0]))/4.0
+
+    # First-order and Second-order derivative
+    fd = np.array([[dx], [dy]])
+    sd = np.array([[dxx, dxy], [dxy, dyy]])
+
+    pm = -np.dot(np.linalg.inv(sd), fd)
+    return (p[0]+pm[0, 0], p[1]+pm[1, 0])
+
+def main(imgname):
+  # Main function for MSOP testing
+  img = cv2.imread(imgname)
+
+  fd = MSOP(pyrLevel=3)
+  kp, desp = fd.detectAndDescribe(img)
+
+  MSOP.drawKeypoints(img, kp, top=100)
+
+  cv2.imwrite(os.path.basename(imgname), img)
+
+  return kp, desp
 
 if __name__=='__main__':
-  kp, nkp = main()
+  if len(sys.argv)==2:
+    kp, desp = main(sys.argv[1])
+  else:
+    kp, desp = main('data/parrington/prtn00.jpg')
   
