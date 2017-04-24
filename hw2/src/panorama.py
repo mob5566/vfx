4e915ff8ea9eecfb19f578d68d1507ff5fe3258e
code14 <mob5566@gmail.com>
2017-04-24 12:20:27 +0800

    Initialize panorama.py

---
+++ hw2/src/panorama.py
@@ -0,0 +1,27 @@
+'''
+# panorama.py -- Build panomara image with given image locations and focal length
+
+* for digital visual effects homework2
+* generate a panorama image by <image_list_file>
+* with each row is image location and its focal length followed
+
+'''
+
+from __future__ import print_function
+
+import os
+import sys
+
+import cv2
+import numpy as np
+
+from msop import MSOP
+
+class Panorama(object):
+  def __init__(self, imglistfile):
+    self.fn = imglistfile       # File name of image list
+
+def main(imgname):
+
+if __name__=='__main__':
+  
