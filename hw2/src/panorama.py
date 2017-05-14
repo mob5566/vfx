297f10990d67fc453cc6d7c80fae00b3e53eedd9
code14 <mob5566@gmail.com>
2017-05-14 20:45:42 +0800

    Add cylindrical projection

--- hw2/src/panorama.py
+++ hw2/src/panorama.py
@@ -4,6 +4,8 @@
 * for digital visual effects homework2
 * generate a panorama image by <image_list_file>
 * with each row is image location and its focal length followed
+* contents
+  * cylindrical projection
 
 '''
 
@@ -14,14 +16,55 @@ import sys
 
 import cv2
 import numpy as np
+from scipy.spatial import KDTree
 
 from msop import MSOP
 
 class Panorama(object):
   def __init__(self, imglistfile):
-    self.fn = imglistfile       # File name of image list
+
+    if not os.path.exists(imglistfile):
+      print("{} doesn't exist!".format(imglistfile))
+      sys.exit(-1)
+
+    # Initialize all images with focal length
+    self.fl = []
+    self.imgs = []
+
+    with open(imglistfile, 'rb') as f:
+      for line in f.readlines():
+        imgfn, fl = line.split()
+        fl = float(fl)
+        
+        self.fl.append(fl)
+        self.imgs.append(Panorama.cylin_project(cv2.imread(imgfn), fl))
+
+  '''
+  Static methods
+  '''
+  # Cylindrical projection
+  @ staticmethod
+  def cylin_project(img, f):
+    assert(type(img)==np.ndarray and len(img.shape)>=2)
+
+    h, w = img.shape[:2]
+
+    x = np.arange(w).reshape(1, -1)
+    for r in xrange(h-1):
+      x = np.append(x, np.arange(w).reshape(1, -1), axis=0)
+
+    y = np.arange(h).reshape(-1, 1)
+    for c in xrange(w-1):
+      y = np.append(y, np.arange(h).reshape(-1, 1), axis=1)
+
+    xm = f*np.tan((x-w/2)/f)+w/2
+    ym = (y-h/2)*np.sqrt((np.tan((x-w/2)/f)**2+1))+h/2
+
+    return cv2.remap(img, xm.astype(np.float32), ym.astype(np.float32), cv2.INTER_LINEAR)
 
 def main(imgname):
+  pass 
 
 if __name__=='__main__':
-  
+  if len(sys.argv) == 2:
+    pano = Panorama(sys.argv[1])
